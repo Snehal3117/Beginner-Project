1015cc659376aea963c46583a70d1c5c2d438708
Snehal3117 <61572063+Snehal3117@users.noreply.github.com>
2021-02-18 16:07:34 +0530

    Update Password_Validator.java

--- Password_Validator.java
+++ Password_Validator.java
@@ -1,7 +1,8 @@
 /* 1. Write method to check whether a string is a valid password. 
-            * Password rules:
-A password must have at least ten characters. A password consists of only letters and digits.
-A password must contain at least two digits. 
+                  * Password rules:
+	A password must have at least ten characters. 
+	A password consists of only letters and digits.
+	A password must contain at least two digits. 
 */
 
 import java.util.Scanner;
